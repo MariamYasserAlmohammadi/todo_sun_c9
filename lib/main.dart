ad579b22b69e4cfec3fa31563d7195d682e44e5b
DESKTOP-Q2BPRDF\NEXT <mariamyasser3092@gmail.com>
2023-10-04 21:37:18 +0300

    + End Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_sun_c9/ui/screens/home/home_screen.dart';
+import 'package:todo_sun_c9/ui/screens/home/splash_screen.dart';
+import 'package:todo_sun_c9/ui/utils/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      routes:{
+        SplashScreen.routeName:(context) => SplashScreen(),
+        HomeScreen.routeName:(context) => HomeScreen(),
+      },
+      initialRoute: SplashScreen.routeName,
+    );
   }
 }
 
---
+++ lib/ui/screens/home/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:todo_sun_c9/ui/screens/home/home_screen.dart';
+import 'package:todo_sun_c9/ui/utils/app_assets.dart';
+
+class SplashScreen extends StatefulWidget{
+  static const String routeName ="splash";
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2),(){
+      Navigator.pushNamed(context, HomeScreen.routeName);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+  return Scaffold(
+body: Image.asset(AppAssets.splash),
+  );
+  }
+}
